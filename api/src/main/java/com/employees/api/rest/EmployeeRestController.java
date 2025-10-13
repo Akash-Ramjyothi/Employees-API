e5500541f944570e837b44a462cf2df690f0eee1
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2025-10-13 20:02:32 +0530

    Added findAll() method

---
+++ api/src/main/java/com/employees/api/rest/EmployeeRestController.java
@@ -0,0 +1,25 @@
+package com.employees.api.rest;
+
+
+import com.employees.api.dao.EmployeeDAO;
+import com.employees.api.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
