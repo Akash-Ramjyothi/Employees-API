0a6b0dab19d0307fa3266964f69caba8f28741ff
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2025-10-14 20:01:40 +0530

    Refactored to Service Layer

--- api/src/main/java/com/employees/api/rest/EmployeeRestController.java
+++ api/src/main/java/com/employees/api/rest/EmployeeRestController.java
@@ -1,8 +1,8 @@
 package com.employees.api.rest;
 
 
-import com.employees.api.dao.EmployeeDAO;
 import com.employees.api.entity.Employee;
+import com.employees.api.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +12,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-    private EmployeeDAO employeeDAO;
 
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-        employeeDAO = theEmployeeDAO;
+    private EmployeeService employeeService;
+
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 }
